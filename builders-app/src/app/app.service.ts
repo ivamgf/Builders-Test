88e592ec813d3a582175094b17f57153ed817bf6
ivamgf <programador.ivam@gmail.com>
2022-10-25 20:12:04 -0300

    settings in routing module - add service and components

---
+++ builders-app/src/app/app.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+}
